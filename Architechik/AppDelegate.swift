6f514feec7406b767e52c8ce952f54f97dafb65e
Alexander <aleksandrcw@gmail.com>
2021-01-02 19:12:15 +0300

    Initial Commit

---
+++ Architechik/AppDelegate.swift
@@ -0,0 +1,39 @@
+//
+//  AppDelegate.swift
+//  Architechik
+//
+//  Created by Александр Цветков on 02.01.2021.
+//  Copyright © 2021 Александр Цветков. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = .white
+        window?.rootViewController = OnboardingViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+    }
+
+
+}
+
---
+++ Architechik/OnboardingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingViewController.swift
+//  Architechik
+//
+//  Created by Александр Цветков on 02.01.2021.
+//  Copyright © 2021 Александр Цветков. All rights reserved.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
